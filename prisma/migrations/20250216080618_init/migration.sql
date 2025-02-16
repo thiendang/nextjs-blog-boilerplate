d7b778fd2053fea7e1a5c7082bbc36e0497b79a7
thiendang <thiendh310@gmail.com>
2025-02-16 15:06:37 +0700

    update prisma schema to v4

---
+++ prisma/migrations/20250216080618_init/migration.sql
@@ -0,0 +1,100 @@
+/*
+  Warnings:
+
+  - You are about to drop the `verification_requests` table. If the table is not empty, all the data it contains will be lost.
+  - The primary key for the `users` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `accounts` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `access_token_expires` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `compound_id` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `provider_id` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `provider_type` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `accounts` table. All the data in the column will be lost.
+  - The primary key for the `sessions` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `access_token` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `sessions` table. All the data in the column will be lost.
+  - Added the required column `provider` to the `accounts` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `accounts` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "verification_requests_token_key";
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "verification_requests";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "verificationtokens" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" TEXT,
+    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Post" ("authorId", "content", "id", "published", "title") SELECT "authorId", "content", "id", "published", "title" FROM "Post";
+DROP TABLE "Post";
+ALTER TABLE "new_Post" RENAME TO "Post";
+CREATE TABLE "new_users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT,
+    "password" TEXT,
+    "email_verified" DATETIME,
+    "image" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+INSERT INTO "new_users" ("created_at", "email", "email_verified", "id", "image", "name", "password", "updated_at") SELECT "created_at", "email", "email_verified", "id", "image", "name", "password", "updated_at" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+CREATE TABLE "new_accounts" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_accounts" ("access_token", "id", "provider_account_id", "refresh_token", "user_id") SELECT "access_token", "id", "provider_account_id", "refresh_token", "user_id" FROM "accounts";
+DROP TABLE "accounts";
+ALTER TABLE "new_accounts" RENAME TO "accounts";
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
+CREATE TABLE "new_sessions" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL,
+    CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_sessions" ("expires", "id", "session_token", "user_id") SELECT "expires", "id", "session_token", "user_id" FROM "sessions";
+DROP TABLE "sessions";
+ALTER TABLE "new_sessions" RENAME TO "sessions";
+CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verificationtokens_token_key" ON "verificationtokens"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verificationtokens_identifier_token_key" ON "verificationtokens"("identifier", "token");
